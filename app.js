b924c8368fa852f7710de2174f85d7b1c930ca0c
unknown <teranihussein@gmail.com>
2022-08-24 13:13:39 +0300

    complete ToDoList Website using Node,js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://Tirani94:boss.2009@cluster0.t55hc.mongodb.net/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
 
 const itemsSchema = {
   name: String
